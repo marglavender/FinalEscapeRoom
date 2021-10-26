34d4f75535d0a715e4c889f17d4ddee8a59aa440
marglavender <89655567+marglavender@users.noreply.github.com>
2021-10-25 23:39:59 -0400

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,23 @@
+//the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+//the modal
+var modal = document.getElementById("myModal");
+
+//the image is inserted inside the modal
+var btn = document.getElementById("myBtn");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+btn.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "https://marglavender.github.io/EscapeRoomNarrative/Map.jpg";
+  captionText.innerHTML = this.alt;
+}
+
+//closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
